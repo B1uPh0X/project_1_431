e4906bf3c3604caf59171db1541d5c38f1ad0574
Simon_Liang <liangs2@sunypoly.edu>
2024-11-05 05:51:33 -0500

    Did the loop for each orbiting body where List takes in Record of Each Orbiting Body and each factor in the orbiting body They are then displayed - waiting on members to add Up to the members to change certain things if only 2D can be done.

--- orbit/bin/main.ml
+++ orbit/bin/main.ml
@@ -29,6 +29,10 @@ let input_float prompt =
 
 ;;
 
+let get_gravitational_constant () =
+  6.674e-11
+;;
+
 let input_int prompt =
   Printf.printf "%s:\n" prompt;
   flush stdout;
@@ -44,27 +48,35 @@ let input_orbiting_body () =
   let inclination = input_float "Enter the inclination (radians)" in
   let argument_of_periapsis = input_float "Enter the argument of periapsis (radians)" in
   let longitude_of_ascending_node = input_float "Enter the longitude of ascending node (radians)" in
-  (* remove let body and everything below in the function so that it would just be 
-  {mass; semi_major_axis; eccentricity; true_anomaly; inclination; argument_of_periapsis; longitude_of_ascending_node}*)
-  let body = {mass; semi_major_axis; eccentricity; true_anomaly; inclination; argument_of_periapsis; longitude_of_ascending_node} in
-  (* This is just so the error doesn't happen in that it's unused yet*)
-  let _ = body.mass in
-  let _ = body.semi_major_axis in
-  let _ = body.eccentricity in
-  let _ = body.true_anomaly in
-  let _ = body.inclination in
-  let _ = body.argument_of_periapsis in
-  let _ = body.longitude_of_ascending_node in
-  body
+  {mass; semi_major_axis; eccentricity; true_anomaly; inclination; argument_of_periapsis; longitude_of_ascending_node}
 ;;
   
 let () = 
-  let _ = input_orbiting_body() in 
   let today = { year = 2024; month = 10; day = 31 } in
   let central_mass = input_float "Enter the mass of the central body (kg)" in
-  let central_body = {mass = central_mass; gravitational_constant = 6.674e-11} in
+  let central_body = {mass = central_mass; gravitational_constant = get_gravitational_constant ()} in
   let num_bodies = input_int "Enter the number of orbiting bodies" in 
   Printf.printf "Today is: %d / %d / %d\n" today.month today.day today.year;
   Printf.printf "Number of orbiting bodies: %d\nGravitational Constant: %f\nCentral Body Mass: %f\n" 
     num_bodies central_body.gravitational_constant central_body.mass;
+  flush stdout;
+  let orbiting_bodies = ref [] in
+  for i = 1 to num_bodies do
+    Printf.printf "\nEnter details for orbiting body %d\n" i;
+    let body = input_orbiting_body () in
+    orbiting_bodies := !orbiting_bodies @ [body]
+  done;
+  for i = 0 to List.length !orbiting_bodies - 1 do
+    let body = List.nth !orbiting_bodies i in
+    Printf.printf "\n- Orbiting Body %d \n
+    - Mass: %f kg \n
+    - Semi-Major Axis: %f meters\n
+    - Eccentricity: %f radians\n
+    - True Anomaly: %f radians\n
+    - Inclination: %f radians\n
+    - Argument of Periapsis: %f radians\n
+    - Longitude of Ascending Node: %f\n"
+      (i + 1) body.mass body.semi_major_axis body.eccentricity body.true_anomaly body.inclination body.argument_of_periapsis body.longitude_of_ascending_node
+  done;
+  ;;
   
